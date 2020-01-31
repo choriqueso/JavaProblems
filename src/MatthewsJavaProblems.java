9269ec32c1ef96a3e0680383d0213684871bfb72
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-31 10:42:16 -0600

    new additions to my class file.

--- src/MatthewsJavaProblems.java
+++ src/MatthewsJavaProblems.java
@@ -1,2 +1,15 @@
 public class MatthewsJavaProblems {
+    public static void main(String[] args) {
+        System.out.println(diff21(10));
+        System.out.println(diff21(24));
+    }
+
+    public static int diff21(int n) {
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return n - 21;
+        }
+    }
+
 }
